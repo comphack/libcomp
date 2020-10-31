1e62e8b26f1d443653d3d6d8fe53641e7e1b3bcc
COMP Omega <compomega@tutanota.com>
2020-10-31 09:05:31 -0600

    [FIX] Encoding of CIconData files.

--- libobjgen/res/VariableStringToEncoding.cpp
+++ libobjgen/res/VariableStringToEncoding.cpp
@@ -1,6 +1,11 @@
 std::vector<char> value = libcomp::Convert::ToEncoding(
     @ENCODING@, @VAR_NAME@);
 
+if(@FIXED_LENGTH@ > 0 && value.size() >= @FIXED_LENGTH@)
+{
+    value.resize(@FIXED_LENGTH@ - 1);
+}
+
 if(!value.empty())
 {
     @ENCODESTREAM@.write(&value[0], static_cast<std::streamsize>(
